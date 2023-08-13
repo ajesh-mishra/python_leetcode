9aca92951db74506059381baf1cd2e62f941d248
Ajesh Mishra <ajesh.sit@gmail.com>
2023-08-13 19:38:48 +0530

    Inital Commit

---
+++ lc_242_valid_anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    @staticmethod
+    def is_anagram(s: str, t: str) -> bool:
+        return sorted(list(s)) == sorted(list(t))
+
+
+if __name__ == "__main__":
+    assert Solution().is_anagram(s="anagram", t="nagaram")
+    assert not Solution().is_anagram(s="rat", t="car")
