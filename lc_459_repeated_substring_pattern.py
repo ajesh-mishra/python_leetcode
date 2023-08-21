c2693672f20276a7102edd0660e04c108b2f36a4
Ajesh Mishra <ajesh.sit@gmail.com>
2023-08-21 22:55:58 +0530

    adds new solution

--- lc_459_repeated_substring_pattern.py
+++ lc_459_repeated_substring_pattern.py
@@ -2,13 +2,17 @@ class Solution:
     @staticmethod
     def repeated_substring_pattern(s: str) -> bool:
         len_s = len(s)
-        for i in range(1, len_s):
+        for i in range(1, (len_s // 2) + 1):
             if len_s % i != 0:
                 continue
             if s[0:i] * (len_s // i) == s:
                 return True
 
         return False
+
+    @staticmethod
+    def repeated_substring_pattern_1(s: str) -> bool:
+        return any([True for i in range(1, (len(s) // 2) + 1) if len(s) % i == 0 and s[0:i] * (len(s) // i) == s])
 
 
 if __name__ == '__main__':
