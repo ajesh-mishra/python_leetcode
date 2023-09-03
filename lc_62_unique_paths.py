6a43625621cbf3bb4c857b75958f9643b6ec642e
Ajesh Mishra <ajesh.sit@gmail.com>
2023-09-03 13:35:23 +0530

    adds new solution

---
+++ lc_62_unique_paths.py
@@ -0,0 +1,18 @@
+class Solution:
+    @staticmethod
+    def unique_paths(m: int, n: int) -> int:
+        cache = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if i == m - 1 and j == n - 1:
+                    cache[i][j] = 1
+                    continue
+                cache[i][j] = cache[i][j + 1] + cache[i + 1][j]
+
+        return cache[0][0]
+
+
+if __name__ == '__main__':
+    assert Solution().unique_paths(m=3, n=2) == 3
+    assert Solution().unique_paths(m=3, n=7) == 28
