9183072472d686d77129614d5bfc5bda7906b695
Ajesh Mishra <ajesh.sit@gmail.com>
2024-01-15 16:59:04 +0530

    adds new solution

---
+++ lc_2600_k_items_with_the_maximum_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def k_items_with_maximum_sum(
+        self, numOnes: int, numZeros: int, numNegOnes: int, k: int
+    ) -> int:
+        if numOnes > k:
+            return k
+        elif numOnes + numZeros > k:
+            return numOnes
+        else:
+            return (numOnes * 2) + numZeros - k
+
+
+if __name__ == "__main__":
+    assert (
+            Solution().k_items_with_maximum_sum(numOnes=3, numZeros=2, numNegOnes=0, k=2) == 2
+    )
+    assert (
+            Solution().k_items_with_maximum_sum(numOnes=3, numZeros=2, numNegOnes=0, k=4) == 3
+    )
